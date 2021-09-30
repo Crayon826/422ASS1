273d50a7b49153280048f531db90e22c50cd2f7c
Crayon826 <71456885+Crayon826@users.noreply.github.com>
2021-09-30 00:24:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,100 +12,101 @@
 *
 ********************************************************************************/ 
 
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const {query,body,params,validationResult} = require('express-validator')
-const RestaurantDB = require('./modules/restaurantDB.js');
-const db = new RestaurantDB();
+ const express = require("express");
+ var cors = require("cors");
+ const app = express();
+ const HTTP_PORT = process.env.PORT || 8080;
+ const RestaurantDB = require("./modules/restaurantDB.js");
+ const db = new RestaurantDB();
+ const dotenv = require("dotenv").config();
+ const { query, validationResult } = require("express-validator");
+ 
+ const mongoLogin = process.env.MONGODB_CONN_STRING;
+ 
+ const connect = db
+   .initialize(mongoLogin)
+   .then(() => {
+     app.listen(HTTP_PORT, () => {
+       console.log(`server listening on: ${HTTP_PORT}`);
+     });
+   })
+   .catch((err) => {
+     console.log(err);
+   });
 
-const cors = require('cors')
-
-app.use(express.json())
-app.use(cors())
-db.initialize(process.env.MONGODB_CONN_STRING)
-	.then(() => {
-		app.listen(process.env.HTTP_PORT, () => {
-			console.log(`server listening on: ${process.env.HTTP_PORT}`);
-		});
-	})
-	.catch(err => { 
-		console.log(err);
-	});
+app.use(express.json());
+app.use(cors());
 app.get('/', async (req, res) => {
-	res.json({ message: 'API Listening' });
+  res.json({ message: 'API Listening' });
 });
 app.post('/api/restaurants', async (req, res, next) => {
-	try {
-		const data = req.body;
-		await db.addNewRestaurant(data);
-		res.json({
-			message: 'success',
-		});
-	} catch (error) {
-		console.log(error);
-		next(error);
-	}
+  try {
+    const data = req.body;
+    await db.addNewRestaurant(data);
+    res.json({
+      message: 'success',
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
-app.get(
-	'/api/restaurants',
-	[query('page'), query('perPage')],
-	async (req, res, next) => {
-		try {
-			const errors = validationResult(req);
-			if (!errors.isEmpty()) {
-				return res.status(400).json({ errors: errors.array() });
-			} 
-			const { page, perPage, borough } = req.query;
-			const allRestaurants = await db.getAllRestaurants(page, perPage, borough);
-			res.json({
-				data: allRestaurants,
-				message: 'success',
-			});
-		} catch (error) {
-			next(error);
-		}
-	}
-);
+app.get('/api/restaurants', [query('page'), query('perPage')], async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { page, perPage, borough } = req.query;
+    const allRestaurants = await db.getAllRestaurants(page, perPage, borough);
+    res.json({
+      data: allRestaurants,
+      message: 'success',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 app.get('/api/restaurants/:_id', async (req, res, next) => {
-	try {
-		const { _id } = req.params;
-		const restaurant = await db.getRestaurantById(_id);
-		res.json(restaurant);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const { _id } = req.params;
+    const restaurant = await db.getRestaurantById(_id);
+    res.json(restaurant);
+  } catch (error) {
+    next(error);
+  }
 });
-app.put('/api/restaurants/:_id', body('_id'), async (req, res, next) => {
-	try {
-		const data = req.body;
-		const { _id } = req.params;
-		await db.updateRestaurantById(data, _id);
-		res.json({
-			message: 'success',
-		});
-	} catch (error) {
-		next(error);
-	}
+
+app.put("/api/restaurants/:id", (req, res) => {
+  db.updateRestaurantById(req.body, req.params.id)
+    .then((data) => {
+      res.status(201).json({
+        message: "data is successfully updated",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "no restaurant data found with id by" + req.params.id,
+      });
+    });
 });
+
 app.delete('/api/restaurants/:_id', async (req, res, next) => {
-	try {
-		const { _id } = req.params;
-		await db.deleteRestaurantById(_id);
-		res.json({
-			message: 'success',
-		});
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const { _id } = req.params;
+    await db.deleteRestaurantById(_id);
+    res.json({
+      message: 'success',
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use((err, req, res, next) => {
-	console.log(err);
-	res.json({
-		code: 500,
-		message:'Internal server error'
-	})
-})
-
-
+  console.log(err);
+  res.json({
+    code: 500,
+    message: 'Internal server error',
+  });
+});
