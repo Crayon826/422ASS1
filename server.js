ce486e17a4e8d4878190ffccd40dc80632f9e19e
Crayon826 <71456885+Crayon826@users.noreply.github.com>
2021-09-30 11:06:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,100 +12,103 @@
 *
 ********************************************************************************/ 
 
- const express = require("express");
- var cors = require("cors");
- const app = express();
- const HTTP_PORT = process.env.PORT || 8080;
- const RestaurantDB = require("./modules/restaurantDB.js");
- const db = new RestaurantDB();
- const dotenv = require("dotenv").config();
- const { query, validationResult } = require("express-validator");
- 
- const MONGO_URL = process.env.MONGODB_CONN_STRING;
- 
- db.initialize(MONGO_URL)
-   .then(() => {
-     app.listen(HTTP_PORT, () => {
-       console.log(`server listening on: ${HTTP_PORT}`);
-     });
-   })
-   .catch((err) => {
-     console.log(err);
-   });
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const HTTP_PORT = process.env.PORT || 8080
+const RestaurantDB = require('./modules/restaurantDB.js')
+const db = new RestaurantDB()
+const dotenv = require('dotenv').config()
+const { query, validationResult } = require('express-validator')
 
-app.use(express.json());
-app.use(cors());
+const MONGO_URL = process.env.MONGODB_CONN_STRING
+
+db.initialize(MONGO_URL)
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`server listening on: ${HTTP_PORT}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+app.use(express.json())
+app.use(cors())
 app.get('/', async (req, res) => {
-  res.json({ message: 'API Listening' });
-});
+  res.json({ message: 'API Listening' })
+})
 app.post('/api/restaurants', async (req, res, next) => {
   try {
-    const data = req.body;
-    await db.addNewRestaurant(data);
+    const data = req.body
+    await db.addNewRestaurant(data)
     res.json({
       message: 'success',
-    });
+    })
   } catch (error) {
-    console.log(error);
-    next(error);
+    console.log(error)
+    next(error)
   }
-});
+})
 app.get('/api/restaurants', [query('page'), query('perPage')], async (req, res, next) => {
   try {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() })
     }
-    const { page, perPage, borough } = req.query;
-    const allRestaurants = await db.getAllRestaurants(page, perPage, borough);
+    let { page, perPage, borough } = req.query
+    page = parseInt(page)
+    page = parseInt(perPage)
+    const allRestaurants = await db.getAllRestaurants(page, perPage, borough)
+    console.log(allRestaurants)
     res.json({
       data: allRestaurants,
       message: 'success',
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 app.get('/api/restaurants/:_id', async (req, res, next) => {
   try {
-    const { _id } = req.params;
-    const restaurant = await db.getRestaurantById(_id);
-    res.json(restaurant);
+    const { _id } = req.params
+    const restaurant = await db.getRestaurantById(_id)
+    res.json(restaurant)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.put("/api/restaurants/:id", (req, res) => {
+app.put('/api/restaurants/:id', (req, res) => {
   db.updateRestaurantById(req.body, req.params.id)
-    .then((data) => {
+    .then(data => {
       res.status(201).json({
-        message: "data is successfully updated",
-      });
+        message: 'data is successfully updated',
+      })
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).json({
-        message: "no restaurant data found with id by" + req.params.id,
-      });
-    });
-});
+        message: 'no restaurant data found with id by' + req.params.id,
+      })
+    })
+})
 
 app.delete('/api/restaurants/:_id', async (req, res, next) => {
   try {
-    const { _id } = req.params;
-    await db.deleteRestaurantById(_id);
+    const { _id } = req.params
+    await db.deleteRestaurantById(_id)
     res.json({
       message: 'success',
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 app.use((err, req, res, next) => {
-  console.log(err);
+  console.log(err)
   res.json({
     code: 500,
     message: 'Internal server error',
-  });
-});
+  })
+})
